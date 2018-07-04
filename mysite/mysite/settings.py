74b0880f12924ac06d56acddc9ae8331c411169b
timlo0416 <timlo0416@gmail.com>
2018-07-04 22:00:26 +0800

    Changed the HTML for the site

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,6 +119,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
