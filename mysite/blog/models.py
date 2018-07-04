825d3f15ef87b4a64a1244200b8d96a2e71b0742
timlo0416 <timlo0416@gmail.com>
2018-07-04 20:25:55 +0800

    My first django web! second commmit.

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    author = models.ForeignKey(User)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(
